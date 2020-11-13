fdbc4dacfe67b7c59733488f18ecc74579039a21
Paulo henrique sene Braz <paulo_braz@unifio.edu.br>
2020-11-13 16:28:43 -0300

    Adicionei variaveis para somar vendas

--- ProjetoTesteGitHub/src/Teste.java
+++ ProjetoTesteGitHub/src/Teste.java
@@ -1,10 +1,12 @@
-
 public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Eae Mundão");
+	
+		int a = 100;
+		int soma = a  +100;
 	}
 
 }
